56438e947f10b0ceab1445470090ce6827c52cd0
alemirellla <72184645@ontinental.edu.pe>
2024-09-03 14:42:01 -0500

    calificacion ingresada

---
+++ main.py
@@ -0,0 +1,16 @@
+calificacion =float(input("Ingrese calificacion: "))
+if(calificacion > 9):
+    print("La calificacion es A.")
+elif(calificacion >8):
+
+    print("La calificacion es B.")
+
+elif(calificacion >= 7.5):
+
+    print("La calificacion es C.")
+
+else:
+    print("La calificacion es R.")
+
+#FIN
+
